c81ee24b5a4bf5d1ac29e9eb47bb16331504a57f
PattyD123 <p.daly@uqconnect.edu.au>
2021-10-08 22:27:42 +1000

    Initial manager program setup

---
+++ manager_system/manager.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <pthread.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+
+/** The Manager 
+ * 
+ * To do:
+ * 
+ * Monitor status of LPR sensors - shared memory
+ * Keep track of where each car is in carpark - hash table
+ * Tell boom gates when to open and close - 
+ * Control what's displayed on information signs at entrances
+ * Ensure room in carpark before letting more cars in - check manager's dynamic memory
+ * Keep track of how full levels are and direct cars to levels that aren't full - check manager's dynamic memory
+ * Keep track of time car has been in parking lot and produce bill once leaves (exit LPR)
+ * 
+ * Display current status of parking lot on frequently updating screen: how full each level is, 
+ * current status of boom gates, signs, temperature sensors and alarms and current car park revenue
+ */
+
+int main(){
+    //open the shared memory - PARKING
+
+
+}
