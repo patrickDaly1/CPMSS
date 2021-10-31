b21b18ce06dc57edc989e65f2688cf2f5f0cb764
Brandon Falconer <39451234+brandonfalconer@users.noreply.github.com>
2021-10-31 20:53:09 +1000

    Add files via upload

--- firealarm_system/firealarm2.c
+++ firealarm_system/firealarm2.c
@@ -6,67 +6,30 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <fcntl.h>
-#include <stdbool.h>
+#include <pthread.h>
+#include "../shared_memory/sharedMemory.h"
 
+int shm_fd;
+const char* key = "PARKING";
+shm* shared_mem;
+size_t shmSize = 2920;
 
-struct boomgate {
-	pthread_mutex_t m;
-	pthread_cond_t c;
-	char s;
-};
+bool alarm_active = false;
+int runs = 1;
+int num_levels = 5;
+int exits = 5;
+int entrances = 5;
+int medianList[30];
 
-struct parkingsign {
-	pthread_mutex_t m;
-	pthread_cond_t c;
-	char display;
-};
+void* tempmonitor(void* ptr) {
 
-void swap(int *xp, int *yp)
-{
-    int temp = *xp;
-    *xp = *yp;
-    *yp = temp;
-}
+	int thread = *((int*)ptr);
 
-/* Source: https://www.geeksforgeeks.org/selection-sort/ */
-void selectionSort(int arr[], int n)
-{
-    int i, j, min_idx;
- 
-    // One by one move boundary of unsorted subarray
-    for (i = 0; i < n-1; i++)
-    {
-        // Find the minimum element in unsorted array
-        min_idx = i;
-        for (j = i+1; j < n; j++)
-        if (arr[j] < arr[min_idx])
-            min_idx = j;
- 
-        // Swap the found minimum element with the first element
-        swap(&arr[min_idx], &arr[i]);
-    }
-}
-
-
-int main()
-{
-	int runs = 1;
-	int levels = 5;
-	int exits = 5;
-	int entrances = 5;
-	
-	int medianList[30];
-
-    int shm_fd;
-    void *shm;
-	
-	shm_fd = shm_open("PARKING", O_RDWR, 0);
-	shm = mmap(0, 2920, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
-
-	//while(true){
-	while(runs < 10) {
+	/* Monitor Temperatures */
+	for (;;) {
 
 		int tempList[35];
+		int medianList[30];
 		int count = 0;
 		int addr;
 		int temp;
@@ -74,112 +37,135 @@ int main()
 		int hightemps;
 		int fixedTempCount;
 
-		bool alarm_active = false;
+		bool first = true;
+		int iterations = 0;
 
-		runs++;
-
-		for(int i = 0; i < levels; i++) {
-
-			/* Monitor Temperatures */
-
-			/* Calculate address of temperature sensor */
-			addr = 0150 * i + 2496;
-
-			bool first = true;
-
-			int j = 0;
-
-			while(count != 30) {
-				j++;
-
-				if(first) {
-					for(int i = 0; i < 5; i++) {
-						temp = *((int16_t *)(shm + addr));
-						tempList[i] = temp;
-						count++;
-						sleep(2);
-					}
-				} else {
-
-					temp = *((int16_t *)(shm + addr));
-					tempList[count] = temp;
-					count++;
-					sleep(2);
-				}
-
-				first = false;
-
-				/* Sort temp list */
-    			int n = sizeof(tempList)/sizeof(tempList[0]);
-    			selectionSort(tempList, n);
-
-				/* Find median */
-				medianTemp = tempList[count - 3];
-				
-				medianList[j] = medianTemp;
-			}
-
-			/* Calculate fixed temperature fire detection */
-			fixedTempCount = 0;
-			for(int i = 0; i < 30; i++) {
-				if(medianList[i] > 58) {
-					fixedTempCount++;
-				}
-			}
+		while (count < 31) {
+			printf("count: %d\n", count);
 			
-			if(fixedTempCount >= 27) {
-				alarm_active = true;
+			temp = shared_mem->levels[thread].tempSen1;
+			//printf("temp: %d sensor:%d\n", temp, i);
+
+			if (first) {
+				for (int i = 0; i < num_levels; i++) {
+					tempList[i] = temp;
+				}
+			}
+			else {
+				tempList[count] = temp;
+				count++;
 			}
 
-			/* Calculate rate of rise fire detection */
-			if((medianList[30] - medianList[0]) > 8) {
-				alarm_active = true;
+			first = false;
+
+			/* Sort temp list using selection sort */
+			int n = sizeof(tempList) / sizeof(tempList[0]);
+			int idx, jdx, min_idx;
+
+			for (idx = 0; idx < n - 1; idx++)
+			{
+				// Find the minimum element in unsorted array
+				min_idx = idx;
+				for (jdx = idx + 1; jdx < n; jdx++)
+					if (tempList[jdx] < tempList[min_idx])
+						min_idx = jdx;
+
+				// Swap the found minimum element with the first element
+				int temp = tempList[min_idx];
+				tempList[min_idx] = tempList[idx];
+				tempList[idx] = temp;
 			}
 
-			/* Activate Alarm */
-			if (alarm_active) {
-				fprintf(stderr, "*** ALARM ACTIVE ***\n");
+			/* Find median */
+			medianTemp = tempList[count - 3];
+			
+
+			medianList[iterations] = medianTemp;
+			iterations++;
+			//printf("median temp: %d at index %d \n", medianList[j], j);
+		}
 		
-				/* Handle the alarm system and open boom gates
-				   Activate alarms on all levels */
-				for (int i = 0; i < levels; i++) {
-					int addr = 0150 * i + 2498;
-					char *alarm_trigger = (char *)shm + addr;
-					*alarm_trigger = 1;
-				}
-				
-				/* Open up all boom gates */
-				for (int i = 0; i < entrances; i++) {
-					int addr = 288 * i + 96;
-					struct boomgate *bg = shm + addr;
-					
-					if (bg->s == 'C') {
-						bg->s = 'R';
-					}
-				}
-				for (int i = 0; i < exits; i++) {
-					int addr = 192 * i + 1536;
-					struct boomgate *bg = shm + addr;
-					
-					if (bg->s == 'C') {
-						bg->s = 'R';
-					}
-				}
-				
-				/* Show evacuation message */
-				char *evacmessage = "EVACUATE ";
-				for (char *p = evacmessage; *p != '\0'; p++) {
-					for (int i = 0; i < entrances; i++) {
-						int addr = 288 * i + 192;
-						struct parkingsign *sign = shm + addr;
-						sign->display = *p;
-					}
-					sleep(20);
-				}
+		/* Calculate fixed temperature fire detection */
+		fixedTempCount = 0;
+		for (int i = 0; i < 30; i++) {
+			//printf("median temp: %d \n", medianList[i]);
+			if (medianList[i] >= 58) {
+				fixedTempCount++;
 			}
 		}
+
+		printf("Fixed temp count: %d\n", fixedTempCount);
+
+		if (fixedTempCount >= 27) {
+			alarm_active = true;
+		}
+
+		/* Calculate rate of rise fire detection */
+		if ((medianList[30] - medianList[0]) > 8) {
+			alarm_active = true;
+		}
+
+		printf("alarm: %d\n", alarm_active);
+
+		usleep(2000);
 	}
-	
-	munmap((void *)shm, 2920);
+}
+
+
+int main()
+{
+	//Locate the segment
+	if ((shm_fd = shm_open(key, O_RDWR, 0)) < 0)
+	{
+		perror("shm_open");
+		return 1;
+	}
+
+	//Attach segment to our data space.
+	if ((shared_mem = (shm*)mmap(0, shmSize, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0)) == (void*)-1)
+	{
+		perror("mmap");
+		return 1;
+	}
+
+	pthread_t threads[num_levels];
+
+	int level[num_levels];
+	for (int i = 0; i < num_levels; i++) {
+
+		level[i] = i;
+		pthread_create(&threads[i], NULL, tempmonitor, &level[i]);
+	}
+
+	for (;;) {
+		runs++;
+
+		/* Activate Alarm */
+		if (alarm_active) {
+			fprintf(stderr, "*** ALARM ACTIVE ***\n");
+
+			/* Handle the alarm system and open boom gates
+			   Activate alarms on all levels */
+			for (int i = 0; i < num_levels; i++) {
+				shared_mem->levels[i].alarm1 = true;
+			}
+
+			/* Show evacuation message */
+			char* evacmessage = "EVACUATE ";
+			for (char* p = evacmessage; *p != '\0'; p++) {
+				for (int i = 0; i < entrances; i++) {
+					pthread_mutex_lock(&shared_mem->entrances[i].SIGN.lock);
+
+					shared_mem->entrances[i].SIGN.display = *p;
+
+					pthread_mutex_unlock(&shared_mem->entrances[i].SIGN.lock);
+				}
+				usleep(20000);
+			}
+		}
+		usleep(1000);
+	}
+
+	munmap(shared_mem, 2920);
 	close(shm_fd);
 }
