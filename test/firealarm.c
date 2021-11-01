17996a005b8ed3869aad76f6fad86ea81ae094d6
PattyD123 <p.daly@uqconnect.edu.au>
2021-11-01 10:33:43 +1000

    Added new and improved sim and firealarm and manager

---
+++ test/firealarm.c
@@ -0,0 +1,165 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <pthread.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <pthread.h>
+#include "sharedMemory.h"
+
+int shm_fd;
+const char* key = "PARKING";
+shm* shared_mem;
+size_t shmSize = 2920;
+
+bool alarm_active = false;
+int num_levels = 5;
+
+void* tempmonitor(void* ptr) {
+	int thread = *((int*)ptr);
+	int temp;
+	temp = shared_mem->levels[thread].tempSen1;
+
+	/* Monitor Temperatures */
+	while(temp != 0) {
+
+		int tempList[35];
+		int medianList[30];
+		int count = 0;
+		// int addr;
+		int medianTemp;
+		// int hightemps;
+		int fixedTempCount;
+
+		// bool first = true;
+		int iterations = 0;
+
+		/* Evaluate the first five temperatures before smoothing*/
+		for (int i = 0; i < num_levels; i++) {
+			temp = shared_mem->levels[thread].tempSen1;
+			tempList[i] = temp;
+		}
+
+		/* Evaluate temps 5-35 for smoothing*/
+		for (count = 5; count < 35; count++) {
+			temp = shared_mem->levels[thread].tempSen1;
+			tempList[count] = temp;
+
+			int temporaryList[5];
+			for (int i = 0; i < 5; i++) {
+				temporaryList[i] = tempList[count - 5 + i];
+			}
+
+			/* Sort temp list using selection sort */
+			int n = sizeof(temporaryList) / sizeof(temporaryList[0]);
+			int idx, jdx, min_idx;
+
+			for (idx = 0; idx < n - 1; idx++)
+			{
+				// Find the minimum element in unsorted array
+				min_idx = idx;
+				for (jdx = idx + 1; jdx < n; jdx++)
+					if (temporaryList[jdx] < temporaryList[min_idx])
+						min_idx = jdx;
+
+				// Swap the found minimum element with the first element
+				int temp = tempList[min_idx];
+				temporaryList[min_idx] = temporaryList[idx];
+				temporaryList[idx] = temp;
+			}
+
+			/* Find median */
+			medianTemp = temporaryList[2];
+			
+
+			medianList[iterations] = medianTemp;
+			iterations++;
+		}
+		
+		/* Calculate fixed temperature fire detection */
+		fixedTempCount = 0;
+		for (int i = 0; i < 30; i++) {
+			//printf("median temp: %d \n", medianList[i]);
+			if (medianList[i] >= 58) {
+				fixedTempCount++;
+			}
+		}
+
+		if (fixedTempCount >= 27) {
+			alarm_active = true;
+		}
+
+		/* Calculate rate of rise fire detection */
+		if ((medianList[30] - medianList[0]) > 8) {
+			alarm_active = true;
+		}
+
+		usleep(2000);
+	}
+	return NULL;
+}
+
+
+int main()
+{
+	/* Locate shared memory segment and attach the segment to the data space*/
+	if ((shm_fd = shm_open(key, O_RDWR, 0)) < 0)
+	{
+		perror("shm_open");
+		return 1;
+	}
+	if ((shared_mem = (shm*)mmap(0, shmSize, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0)) == (void*)-1)
+	{
+		perror("mmap");
+		return 1;
+	}
+
+	/* Create a thread for each level */
+	pthread_t threads[num_levels];
+
+	int level[num_levels];
+	for (int i = 0; i < num_levels; i++) {
+
+		level[i] = i;
+		pthread_create(&threads[i], NULL, tempmonitor, &level[i]);
+	}
+
+	while(shared_mem->levels[0].tempSen1 >= 0) {
+		/* Activate Alarm */
+		if (alarm_active) {
+			fprintf(stderr, "*** ALARM ACTIVE ***\n");
+
+			/* Handle the alarm system and open boom gates
+			   Activate alarms on all levels */
+			for (int i = 0; i < num_levels; i++) {
+				shared_mem->levels[i].alarm1 = true;
+			}
+
+			/* Show evacuation message */
+			char* evacmessage = "EVACUATE ";
+			for (char* p = evacmessage; *p != '\0'; p++) {
+				for (int i = 0; i < num_levels; i++) {
+					pthread_mutex_lock(&shared_mem->entrances[i].SIGN.lock);
+
+					shared_mem->entrances[i].SIGN.display = *p;
+					pthread_cond_signal(&shared_mem->entrances[i].SIGN.condition);
+
+					pthread_mutex_unlock(&shared_mem->entrances[i].SIGN.lock);
+				}
+				usleep(20000);
+			}
+			alarm_active = false;
+		}
+		else {
+			for (int i = 0; i < num_levels; ++i) {
+				shared_mem->levels[i].alarm1 = false;
+			}
+		}
+		usleep(1000);
+	}
+
+	munmap(shared_mem, 2920);
+	close(shm_fd);
+}
---
+++ test/sharedMemory.h
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <string.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdbool.h>
+
+#define NUM_ENTRANCES 5
+#define NUM_EXITS 5
+#define NUM_LEVELS 5
+
+//boomgate
+typedef struct boom_gate {
+    //pthread_mutex_t for boom gate
+    pthread_mutex_t lock;
+    pthread_cond_t condition;
+    char status;
+} boom_gate_t;
+
+//LPR
+typedef struct lpr {
+    pthread_mutex_t lock;
+    pthread_cond_t condition;
+    char rego[6];
+} lpr_t;
+
+//Information Sign
+typedef struct sign {
+    pthread_mutex_t lock;
+    pthread_cond_t condition;
+    char display;
+} sign_t;
+
+//entrance
+typedef struct entrance {
+    lpr_t LPR;
+    boom_gate_t BG;
+    sign_t SIGN;
+} entrance_t;
+
+//exit
+typedef struct exit {
+    lpr_t LPR;
+    boom_gate_t BG;
+} exit_t;
+
+//level
+typedef struct level {
+    lpr_t LPR;
+    // boom_gate_t BG;
+    short tempSen1; //correct? ** think so **
+    bool alarm1; //correct? ** think so **
+} level_t;
+
+//Shared memory
+typedef struct shm {
+    entrance_t entrances[NUM_ENTRANCES];
+    exit_t exits[NUM_EXITS];
+    level_t levels[NUM_LEVELS];
+} shm;
